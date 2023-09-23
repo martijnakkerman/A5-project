c18bed2c8409ec3d94bc5bb8ac1a9743fe8020d5
marti <martiak@live.nl>
2023-09-23 16:22:17 +0200

    changed file name to capital letter for consistency

--- app/Models/Embed.php
+++ app/Models/Embed.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class band extends Model
+class Embed extends Model
 {
     use HasFactory;
 }
