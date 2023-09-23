49b6359010f15f6bbd40d85d4ddbf02e312af33a
marti <martiak@live.nl>
2023-09-23 16:05:14 +0200

    changed users to user and added admin role.

--- database/migrations/2014_10_12_000000_create_user_table.php
+++ database/migrations/2014_10_12_000000_create_user_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('user', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->boolean('is_admin');
         });
     }
 
