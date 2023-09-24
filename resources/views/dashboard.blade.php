30fdff6ad1daae096ee97e8ae72bfdd1a3b8db77
marti <martiak@live.nl>
2023-09-24 18:22:06 +0200

    Added band manager table to dashboard.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,21 +8,51 @@
                 <div class="card-header">{{ __('User Information') }}</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-                        {{ Auth::user()->name }} <br>
-                        {{ Auth::user()->email }} <br>
-                        <a class="btn btn-primary" href="{{ route('user.edit')}}">Edit user information</a>
+                        <p>Username: {{ Auth::user()->name }}</p>
+                        <p>E-mail: {{ Auth::user()->email }}</p>
+                        <a class="btn btn-primary" href="{{ route('user.edit')}}">Edit user information</a> <br><br>
+                        <button class="btn btn-primary">Reset Password</button>
+                </div>
+            </div>
 
-                        <p>Managed bands:</p>
+            <br>
+
+            <div class="card">
+                <div class="card-header">{{ __('Managed bands') }}</div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <table class="table table-striped">
+                                <thead>
+                                <tr>
+                                    <td>Your bands</td>
+                                    <td>Last changed</td>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                     @foreach(Auth::user()->bands as $band)
+                                         <tr>
+                                             <td>{{$band->name}}</td>
+                                             <td>{{$band->updated_at}}</td>
+                                             <td><a href="{{route('band.edit')}}">manage</a></td>
+                                         </tr>
+                                     @endforeach
+                                </tbody>
+                            </table>
+
+
 
 
 
                 </div>
             </div>
+
+
+
+
+
+
+
         </div>
     </div>
 </div>
