c01999617f853f75d4f5b8c3b64ee572e4819a88
marti <martiak@live.nl>
2023-10-01 15:49:21 +0200

    changed the requests into a seperate folder to clean up the code. dedicated requests are in there now.

--- app/Http/Controllers/Band/BandController.php
+++ app/Http/Controllers/Band/BandController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Band;
 
+use App\Http\Requests\ValidateBandRequest;
 use App\Models\Embed;
 use App\Models\Band;
 use Illuminate\Http\Request;
@@ -31,17 +32,8 @@ class BandController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ValidateBandRequest $request)
     {
-        $request->validate([
-            'name'=>'required|string',
-            'description'=>'required|string',
-            'biography'=>'required|string',
-            'image'=>'required|image|mimes:jpg,png',
-            'text_color'=>'nullable|string',
-            'background_color'=>'nullable|string',
-        ]);
-
         $uploadedfile = Storage::disk('public')->put("images", $request->image);
         $request_all = $request->all();
         $request_all['image_path'] = "images/".basename($uploadedfile);
@@ -53,7 +45,7 @@ class BandController extends Controller
         $band->embeds()->save($embed);
     }
         return redirect('/dashboard')
-            ->with('success', 'Band information updated');
+            ->with('success', 'Band: '.$band->name.' was succesfully created.');
     }
 
     /**
@@ -75,16 +67,8 @@ class BandController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Band $band)
+    public function update(ValidateBandRequest $request, Band $band)
     {
-        $request->validate([
-            'name'=>'required|string',
-            'description'=>'required|string',
-            'biography'=>'required|string',
-            'image'=>'nullable|image|mimes:jpg,png',
-            'text_color'=>'nullable|string',
-            'background_color'=>'nullable|string',
-        ]);
         $request_all = $request->all();
         if(!is_null($request->image)) {
             $uploadedfile = Storage::disk('public')->put("images", $request->image);
@@ -93,13 +77,15 @@ class BandController extends Controller
         $band->update($request_all);
         $band->users()->sync([(Auth::user()->id)]);
         $band->embeds()->delete();
+
         foreach ($request->youtube_url as $youtube_url) {
             $embed = Embed::create(['youtube_url' => $youtube_url, 'band_id' => $band->id]);
             $band->embeds()->save($embed);
         }
+
         $band->update($request->all());
         return redirect('/dashboard')
-            ->with('success', 'Band information updated');
+            ->with('success', 'Band information updated.');
     }
 
 
@@ -109,9 +95,18 @@ class BandController extends Controller
     public function destroy(Band $band)
     {
         $bandname = $band->name;
+
+        if (Storage::disk('public')->exists($band->image_path)) {
+
+            Storage::disk('public')->delete($band->image_path);
+        } else {
+            dd("file not found");
+        }
+
         $band->users()->detach();
+        $band->embeds()->delete();
         $band->delete();
         return redirect("/dashboard")
-            ->with('success', 'Your band: '.$bandname.' has been deleted');
+            ->with('success', 'Your band: '.$bandname.' has been deleted.');
     }
 }
