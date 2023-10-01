d92f004ca31b7d5e1892fb07d5061004bf61d26c
marti <martiak@live.nl>
2023-10-01 19:34:49 +0200

    added the search functionality

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Band;
 use Illuminate\Http\Request;
-
 class HomeController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('home');
+        $search = $request->input('search'); // Get the user's search input
+
+        // Perform a database query to search for bands by title or description
+        $bands = Band::where('name', 'like', "%$search%")
+            ->orWhere('description', 'like', "%$search%")
+            ->orderBy("created_at","desc")->paginate(9);
+
+        return view('home', compact('bands'));
     }
+
+
 }
